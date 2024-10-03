4e17a9705d01b426ea88955fbc9c7e76fef9e0e6
TukeCS <tuke.richards@gmail.com>
2024-10-03 12:16:00 +0200

    UwU

---
+++ script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("clickable-image")
+  .addEventListener("click", function (e) {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.textContent = "❤️ je t'aime ❤️";
+    document.body.appendChild(heart);
+
+    // Position the heart near the mouse click
+    heart.style.left = `${e.clientX - 20}px`;
+    heart.style.top = `${e.clientY - 20}px`;
+
+    // Remove the heart after animation
+    setTimeout(() => heart.remove(), 1000);
+  });
